6c4a7eccd26b782c087ec06e0d23c1cb4b0ddf41
eschmit <eschmit@groceryshopping.net>
2015-01-06 16:05:10 -0600

    corrected mount directory (more)

--- admin.sh
+++ admin.sh
@@ -2,25 +2,25 @@
 #GSN WP Host (admin) Setup
 #Version 1.0.0
 
-echo "************ create a file system and folder on the elastic block store volume (disk)"
+echo "************ create a file system and folder on the elastic block store volume (disk)\r"
 mkfs -t ext4 /dev/xvdf
 
-echo "************ create a mount point"
+echo "************ create a mount point\r"
 mkdir /usr/share/nginx/wordpress
 
-echo "************ mount the attached ebs to /wordpress"
+echo "************ mount the attached ebs to /wordpress\r"
 mount /dev/xvdf /usr/share/nginx/wordpress
 
-echo "************ nfs requirements"
+echo "************ nfs requirements\r"
 apt-get install nfs-kernel-server
 
-echo "************ make the directory to share"
-mkdir /dev/xvdf/wordpress
+echo "************ make the directory to share\r"
+mkdir /dev/xvdf/nginx/wordpress
 
-echo "************ change the ownership"
+echo "************ change the ownership\r"
 chown nobody:nogroup /dev/xvdf/nginx/wordpress
 
-echo "************ give permission to access the drive to the worker(s)"
+echo "************ give permission to access the drive to the worker(s)\r"
 echo "/dev/xvdf/nginx/wordpress 0.0.0.0(rw,sync,no_subtree_check)" &> /etc/exports
 
 service nfs-kernel-server start
